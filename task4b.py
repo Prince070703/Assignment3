03c24a248a30978e9cbc4ff503f4ce84e113404b
Prince070703 <pkabc1738@gmail.com>
2025-07-11 15:37:31 +0530

    Add files via upload

---
+++ task4b.py
@@ -0,0 +1,9 @@
+import math
+n = float(input('Enter the number :'))
+square_result = math.sqrt(n)
+log_result = math.log(n)
+sin_result = math.sin(n)
+
+print('The square is :' , square_result)
+print('The log is :' , log_result)
+print('The sin value is :' , sin_result)   
